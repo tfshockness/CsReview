6a9a8d86cf4452322a84ebebd81070d713a3fa24
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2017-10-22 01:18:13 -0700

    End of CHP3
    No examples for Partial Class, Partial Methods, and Extension Methods.

--- CHP3/Struct.cs
+++ CHP3/Struct.cs
@@ -4,12 +4,29 @@
 
 //Usually struct are used for small data types. Performance reasons.
 
+//Struct does not support inheritance
 namespace CsReview.Chp3
 {
     struct CustomerStruct
     {
-        public string FirstName;
-        public string LastName;
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
 
+        //THE DEFAULT CONSTRUCTOR is always present, even when i have a overload constructor.
+        public CustomerStruct(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
+
+//Struct can be initialized:
+//CustomerStruct customer;
+//FirstName = "Tim"
+
+//Or
+//CustomerStruct customer = new CustomerStruct();
+//New key word will just call the constructor, it will not put the struct in the heap,
+//and all structs values are initialized with default values;
+//All the fields should be initialized before use. Same rule of data types.
